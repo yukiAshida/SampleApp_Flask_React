069fd39d5c2a3e5937f5652f8ed8c79344ba2c8a
yukiAshida <ashidayuki99@gmail.com>
2021-03-10 14:44:47 +0900

    status cleen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,61 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index';
 
-import RootPage from './root'
-import ComponentSamplesPage from './component_samples'
-import AjaxSamplesPage from './ajax_samples'
+import RootPage from './root';
+import ComponentSamplesPage from './component_samples';
+import AjaxSamplesPage from './ajax_samples';
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 'root',
+        };
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      "page": "root",
-    };
-  }
+    // トップレベルのページ遷移・データ更新関数
+    // [TODO] pageがundifinedになりうる
+    transPage({ nextpage, update = {} }) {
+        // 状態をコピー
+        var new_state = Object.assign({}, this.state);
 
-  // トップレベルのページ遷移・データ更新関数
-  // [TODO] pageがundifinedになりうる
-  transPage({nextpage, update={}}){
-        
-    // 状態をコピー
-    var new_state = Object.assign({},this.state);
-    
-    // 遷移先のページを指定
-    new_state["page"] = nextpage;
+        // 遷移先のページを指定
+        new_state['page'] = nextpage;
 
-    // 親パラメタの更新
-    for(var key in update){  new_state[key] = update[key];  }
+        // 親パラメタの更新
+        for (var key in update) {
+            new_state[key] = update[key];
+        }
 
-    // 状態を遷移
-    this.setState(new_state);
+        // 状態を遷移
+        this.setState(new_state);
+    }
 
-  }
+    render() {
+        console.log('page:', this.state['page']);
 
-  render(){
+        return (
+            <div id="index">
+                {/* rootページ */}
+                {this.state['page'] == 'root' && (
+                    <RootPage
+                        transPage={({ nextpage, update }) => this.transPage({ nextpage, update })}
+                    />
+                )}
 
-    console.log("page:", this.state["page"]);
+                {/* component_samplesページ */}
+                {this.state['page'] == 'component_samples' && (
+                    <ComponentSamplesPage
+                        transPage={({ nextpage, update }) => this.transPage({ nextpage, update })}
+                    />
+                )}
 
-    return(
-        <div id="index">
-
-            {/* rootページ */}
-            {this.state["page"]=="root" && <RootPage transPage={({nextpage, update})=>this.transPage({nextpage, update})} />}
-
-            {/* component_samplesページ */}
-            {this.state["page"]=="component_samples" && <ComponentSamplesPage transPage={({nextpage, update})=>this.transPage({nextpage, update})} />}
-
-            {/* component_samplesページ */}
-            {this.state["page"]=="ajax_samples" && <AjaxSamplesPage transPage={({nextpage, update})=>this.transPage({nextpage, update})} />}
-            
-
-        </div>
-    );
-  } 
+                {/* component_samplesページ */}
+                {this.state['page'] == 'ajax_samples' && (
+                    <AjaxSamplesPage
+                        transPage={({ nextpage, update }) => this.transPage({ nextpage, update })}
+                    />
+                )}
+            </div>
+        );
+    }
 }
 
-
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app')
-);
+ReactDOM.render(<Main />, document.getElementById('app'));
--- frontend/src/root.js
+++ frontend/src/root.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import {Button} from './component/buttons';
+import React from "react";
+import { Button } from "./component/buttons";
 
 export default class RootPage extends React.Component {
-  
-    constructor(props){
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    transPage(e, nextpage){
-        this.props.transPage({"nextpage":nextpage});
-    }
+  transPage(e, nextpage) {
+    this.props.transPage({ nextpage: nextpage });
+  }
 
-    render(){
-        
-        return(
-            <div id="root_page" className="flex-row-center">
-                
-                <div className="mycard flex-column-center">
-                    <Button color="BLUE" onClick={(e)=>this.transPage(e, "component_samples")} width="50vw">【サンプル】コンポーネント</Button>
-                    <Button color="BLUE" onClick={(e)=>this.transPage(e, "ajax_samples")} width="50vw">【サンプル】AJAX</Button>
-                </div>
-
-            </div>
-        );
-    }
-    
+  render() {
+    return (
+      <div id="root_page" className="flex-row-center">
+        <div className="mycard flex-column-center">
+          <Button
+            color="BLUE"
+            onClick={(e) => this.transPage(e, "ajax_samples")}
+            width="50vw"
+          >
+            【サンプル】AJAX
+          </Button>
+          <Button
+            color="BLUE"
+            onClick={(e) => this.transPage(e, "component_samples")}
+            width="50vw"
+          >
+            【サンプル】コンポーネント
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
-
